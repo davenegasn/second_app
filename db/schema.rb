f23afd2c684df87c83d25feb9a3ff5ce7a18ff2c
davenegasn <davenegasn@gmail.com>
2018-08-22 20:49:49 -0300

    Tercer commit previo a agregar campo email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822221855) do
+ActiveRecord::Schema.define(version: 20180822234815) do
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
